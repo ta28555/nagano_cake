fed7887e508fbdba7402eb4fbfab39af1792b174
ta28555 <no.yuuuuu@gmail.com>
2021-05-24 15:20:02 +0000

    [add]admin information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create!(
+  last_name: '管理者',
+  first_name: '管理者',
+  last_name_kana: 'カンリシャ',
+  first_name_kana: 'カンリシャ',
+  email: 'admin@gmail.com',
+  postal_code: '0000000',
+  address: 'nagano cake',
+  telephone_number: '00000000000',
+  is_active: false,
+  created_at: '2000-01-01',
+  updated_at: '2000-01-01',
+  password: 'adminpass',
+  password_confirmation: 'adminpass',
+  admin: true
+)
