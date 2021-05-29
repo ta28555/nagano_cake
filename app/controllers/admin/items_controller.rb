6145b6b957532200dfe5a6b313e66e682178a934
ta28555 <no.yuuuuu@gmail.com>
2021-05-29 10:19:58 +0000

    [Finish]genre index

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item =Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path
   end
 
   def show
