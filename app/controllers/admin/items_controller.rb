2dddc227f084ce3498daac948c69cd9d9d5ff364
ta28555 <no.yuuuuu@gmail.com>
2021-06-01 02:41:57 +0000

    [Update]item show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,12 +10,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item =Item.new(item_params)
-    item.save
-    redirect_to admin_item_path(:id)
+    @item =Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '71caf905ff81adf2713da5258c915d144660cec195e934c1e571ab777d24f7f360b4218de3a48eaa8a31d2ab68a574fc4ac5cc219ece83fcc30c6a9ff5c69f02'
