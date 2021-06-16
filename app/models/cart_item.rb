86b125a6413926544238ed3f3ae83cc0f9d1710d
ta28555 <no.yuuuuu@gmail.com>
2021-06-16 15:01:19 +0000

    [Update]public item_cart index

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,12 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
 
+  def tax_price
+    (self.item.price * 1.10 ).round
+  end
+
+  def subtotal
+    amount * tax_price
+  end
+
 end
