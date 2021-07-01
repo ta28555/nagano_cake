83c05b2dffafccdf9ab2825f71485557d24105c9
ta28555 <no.yuuuuu@gmail.com>
2021-07-01 04:33:32 +0000

    [Update]public cart

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -44,7 +44,7 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save!
-    redirect_to orders_confirm_path
+    redirect_to orders_thanks_path
   end
 
   def index
