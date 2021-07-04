17541e858e88344cfbc1d30eec84ec68ba861aec
ta28555 <no.yuuuuu@gmail.com>
2021-07-04 12:47:01 +0000

    [Update]mypage

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,7 +9,10 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   def name
-    last_name + first_name
+    "#{last_name} #{first_name}"
+  end
+  def kana
+    "#{last_name_kana} #{first_name_kana}"
   end
 
 end
