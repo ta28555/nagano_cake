80a660783c18cd1758973636432ce56b9a163e76
ta28555 <no.yuuuuu@gmail.com>
2021-07-06 15:14:36 +0000

    [Update]mypage edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,12 +9,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_parems)
+    redirect_to customers_my_page_path
   end
 
   def confirm
+    @customer = current_customer
+    
   end
 
   def unsubscribe
+    redirect_to root_path
+  end
+
+  private
+  def customer_parems
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :telephone_number, :is_active)
+
   end
 
 end
