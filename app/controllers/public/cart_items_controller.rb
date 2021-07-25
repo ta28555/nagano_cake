7fe6621ec551dc79bca76e37d244c4d07d5fb7be
ta28555 <no.yuuuuu@gmail.com>
2021-07-25 16:25:39 +0000

    [Update]order detail

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -17,6 +17,8 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
--- app/models/order_ditail.rb
+++ app/models/order_ditail.rb
@@ -3,6 +3,14 @@ class OrderDitail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
+  def tax_price
+    (self.item.price * 1.10 ).round
+  end
+
+  def subtotal
+    amount * tax_price
+  end
+
   enum making_status: {
     not_possible: 0, # 製作不可
     waiting: 1, # 製作待ち
