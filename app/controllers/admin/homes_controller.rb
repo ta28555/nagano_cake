a450dafc1c097eda7fd3ef82744c349ee9ee8e91
ta28555 <no.yuuuuu@gmail.com>
2021-07-27 16:56:46 +0000

    [Update]admin order detiail

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    @order = Order.all
+    @order = Order.page(params[:page]).per(10).order('id DESC')
   end
 
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,26 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+
+    #@order_ditail= OrderDitail.find(params[:id])
+    #@order_ditail.update(order_ditail_params)
+
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :total_payment, :postage, :payment_method, :status)
+  end
+
+  def order_ditail_params
+    params.require(:order_ditail).permit(:item_id, :order_id, :price, :amount, :making_status)
   end
 
 end
