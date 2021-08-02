3679f0af40cfc9a020a5e73a1999aecbdf8e019e
ta28555 <no.yuuuuu@gmail.com>
2021-08-02 01:27:58 +0000

    [Update]order status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,6 +3,14 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_ditail = OrderDitail.find(params[:id])
     @order_ditail.update(order_ditail_params)
+    order_ditails = @order_ditail.order.order_ditails
+
+    if order_ditails.where(making_status: "in_creation").count >= 1
+      @order_ditail.order.update(status: 2)
+    elsif order_ditails.where(making_status: "complete").count == order_ditails.count
+      @order_ditail.order.update(status: 3)
+    end
+
     redirect_to admin_order_path(@order_ditail.order)
   end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,6 +7,13 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+
+    if @order.status == "confirmed"
+      @order.order_ditails.each do |order_ditail|
+        order_ditail.update(making_status: 1)
+      end
+    end
+
     redirect_to admin_order_path(@order)
   end
 
