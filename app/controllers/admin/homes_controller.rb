fb43996a83eb5330edea937068699016c4cc6ff6
ta28555 <no.yuuuuu@gmail.com>
2021-08-03 15:47:07 +0000

    [Update]admin order index

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,11 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    @order = Order.page(params[:page]).per(10).order('id DESC')
+    if params[:customer_id].present?
+      @order = Order.where(customer_id: params[:customer_id]).page(params[:page]).per(10).order('id DESC')
+    else
+      @order = Order.all.page(params[:page]).per(10).order('id DESC')
+    end
   end
 
 end
